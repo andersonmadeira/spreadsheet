1d11aedc70f4bc301ec686300dc79fe16c758ab2
Anderson Madeira <andersonmadeiracs@gmail.com>
2020-09-14 23:05:15 -0300

    Basic Editable cells

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import styles from './styles/App.module.scss'
 function App() {
   return (
     <div className={styles.app}>
-      <Sheet lines={5} columns={6} />
+      <Sheet lines={50} columns={10} />
     </div>
   )
 }
--- src/components/CellData.jsx
+++ src/components/CellData.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import classnames from 'classnames'
 import styles from '../styles/CellData.module.scss'
 
-const CellData = ({ id, selected, value, handleSelection }) => {
+const CellData = ({ id, selected, handleSelection }) => {
+  const [cellValue, setCellValue] = useState('')
+  const [isInEditionMode, setIsInEditionMode] = useState(false)
+  const cellRef = useRef()
+
   return (
     <div
+      ref={cellRef}
       className={classnames(
         styles.cell_data,
         selected ? styles.selected : null,
       )}
+      contentEditable={isInEditionMode}
       role="presentation"
       onClick={() => handleSelection(id)}
+      onDoubleClick={() => {
+        setIsInEditionMode(!isInEditionMode)
+      }}
     >
-      <span className={styles.cell_data__anchor} />
-      {value}
+      {isInEditionMode ? (
+        <input
+          style={{ width: '100%' }}
+          autoFocus
+          className={styles.value_editor}
+          value={cellValue}
+          onChange={(event) => setCellValue(event.target.value)}
+          onBlur={() => setIsInEditionMode(false)}
+        />
+      ) : (
+        <span className={styles.value_viewer}>{cellValue}</span>
+      )}
     </div>
   )
 }
